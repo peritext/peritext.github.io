790242446a115810fd38d30727c885e775b15bdb
Robin de Mourat <robin.demourat@gmail.com>
2018-08-18 18:37:32 +0200

    setup redirect

--- _next/4db54d56-2400-4db2-a734-b3172c630b29/page/section/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/section', function() {
-            var comp = module.exports=webpackJsonp([8],{1330:function(e,t,u){e.exports=u(1331)},1331:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var i=u(1),a=function(e){return e&&e.__esModule?e:{default:e}}(i);Object.defineProperty(t,"__esModule",{value:!0});var d=u(123),o=n(d),r=u(150),l=n(r),c=u(89),f=u(520),s=n(f),p=c.templates.web.Section;t.default=function(e){var t=e.url.query;return t.contextualizationId&&setTimeout(function(){o.default.replace("/section?id="+t.id,"/section/"+t.id)},1e3),a.default.createElement(l.default,{activeViewId:t.id},a.default.createElement(p,{id:t.id,citations:s.default,query:t}))}}},[1330]);
-            return { page: comp.default }
-          })
-        
